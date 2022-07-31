78d14c700c65f3899abb587aea538c98272971b0
L2J <leedjlee@naver.com>
2022-07-31 17:35:52 +0900

    Lombok 관련 수정

--- CRUD/src/main/java/com/crud/domain/BoardVO.java
+++ CRUD/src/main/java/com/crud/domain/BoardVO.java
@@ -1,50 +1,20 @@
 package com.crud.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
 import java.util.Date;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
 public class BoardVO {
-	private Integer bno;
-	private String title;
-	private String content;
-	private String writer;
-	private Date regdate;
-	private int viewcnt;
-	public Integer getBno() {
-		return bno;
-	}
-	public void setBno(Integer bno) {
-		this.bno = bno;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public Date getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(Date regdate) {
-		this.regdate = regdate;
-	}
-	public int getViewcnt() {
-		return viewcnt;
-	}
-	public void setViewcnt(int viewcnt) {
-		this.viewcnt = viewcnt;
-	}
-	
-	
+	Integer bno;
+	String title;
+	String content;
+	String writer;
+	Date regdate;
+	int viewcnt;
 }
